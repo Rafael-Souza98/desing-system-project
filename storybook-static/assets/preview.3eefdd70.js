88ff765563e685624287c12f9a73ba3066a06961
Rafael Souza <faelsos98@gmail.com>
2022-10-14 11:14:22 -0300

    ci: Add build storybook for deploy

---
+++ storybook-static/assets/preview.3eefdd70.js
@@ -0,0 +1,2 @@
+import{t as o}from"./index.990ee196.js";import"./iframe.eb7051a4.js";import"./es.map.constructor.e3458e6d.js";import"./es.number.to-fixed.4a8e841c.js";import"./index.ff335c70.js";const s={actions:{argTypesRegex:"^on[A-Z].*"},controls:{matchers:{color:/(background|color)$/i,date:/Date$/}},docs:{theme:o.dark}};export{s as parameters};
+//# sourceMappingURL=preview.3eefdd70.js.map
